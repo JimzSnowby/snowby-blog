fd99ba37c04391057f67eac00eba65078658c3a9
JimzSnowby <JimzSnowby@users.noreply.github.com>
2024-10-26 16:49:33 +0000

    deploy: 79261d8849694db475feb39d79046a52e1eba7c5

---
+++ assets/27-10-2024-VBn22sTX.js
@@ -0,0 +1,27 @@
+const e=`<h1>Building my website</h1>
+<h2>Project Aims</h2>
+<p>To build this website! The goal is to create a simple portfolio/blog that showcases who I am. This project will allow me to post individual articles and display them in an easy-to-use layout, and there will also be components that lead to my LinkedIn and GitHub profiles, as well as a page that gives visitors a background into who I am.</p>
+<p>The project needs to be scalable, allowing me to add new features in the future without affecting the rest of the website, and also needs to have a responsive design, meaning it is viewable on all browsers, as well as pc's, phones, and tablets.</p>
+<h2>Technology and Reasoning</h2>
+<p>This website is built using:</p>
+<ul>
+<li><strong>JavaScript</strong>:
+During my web development modules at university, we learnt JavaScript, so I'm using it here because I am comfortable with it and it is also one of the most popular languages used for building websites.
+<img src="https://static.vecteezy.com/system/resources/previews/027/127/463/non_2x/javascript-logo-javascript-icon-transparent-free-png.png" alt="JavaScript Logo"></li>
+<li><strong>React</strong>:
+A JavaScript library that I used at university which provides an easy way of building a UI. React allows me to design components of a page which can update without needing to reload the entire page. With this component base structure, I can easily follow <a href="https://en.wikipedia.org/wiki/Object-oriented_programming">OOP</a> principles by keeping components separate and can add or adjust them as needed. This also lets me follow <a href="https://en.wikipedia.org/wiki/SOLID">SOLID</a> principle of single responsibility.
+<img src="https://static-00.iconduck.com/assets.00/react-original-wordmark-icon-840x1024-vhmauxp6.png" alt="React Logo"></li>
+<li><strong>Vite</strong>:
+Pronounced &quot;Veet&quot;, Vite is a framework that I used alongisde React at university which helps set up and run web projects quickly. It makes working with tools like React faster and easier, and quickly builds and displays projects in the browser as you work on it, so any changes can be seen instantly, which helps speed up the creation process.
+<img src="https://upload.wikimedia.org/wikipedia/commons/thumb/f/f1/Vitejs-logo.svg/2078px-Vitejs-logo.svg.png" alt="Vite Logo"></li>
+<li><strong>Tailwind</strong>:
+A CSS framework for styling websites. Rather than traditional CSS, where you need to write the styling rules for each element, Tailwind provides pre-made classes to style components directly. This makes it very fast to style components on the fly, and it is very customisable.
+<img src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/Tailwind_CSS_Logo.svg/2560px-Tailwind_CSS_Logo.svg.png" alt="Tailwind Logo"></li>
+<li><strong>Markdown</strong>:
+Rather than typing out these articles directly into HTML or fetching from a database/API (which GitHub Pages doesn't support), I wanted to be able to write articles with very little hassle. After a bit of research, I learnt about Markdown, a super simple way to format text into HTML using plain characters, and after writing a simple JavaScript plugin for Vite, I can now convert .md files into what you're reading now!
+<img src="https://markdown-here.com/img/icon256.png" alt="Markdown Logo"></li>
+<li><strong>GitHub Pages</strong>:
+As a poor, unemployed graduate, so I wasn't really willing to spend money on server hosting, but I also wanted to challenge myself. I originally looked into using a Raspberry Pi as a web server, until I came across <a href="https://pages.github.com/">GitHub Pages</a>. It's free and easy to use, although there are drawbacks such as not being able to run any server side code (so PHP was out of the question).
+<img src="https://cdn-icons-png.flaticon.com/512/25/25231.png" alt="GitHub Logo"></li>
+</ul>
+`;export{e as default};
