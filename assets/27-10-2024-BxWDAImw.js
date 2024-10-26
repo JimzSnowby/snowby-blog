ab31d6107b13c240f4da4a69aaa1762ff0f8ef8c
JimzSnowby <JimzSnowby@users.noreply.github.com>
2024-10-26 12:58:10 +0000

    deploy: 10d9687a3576b9f9006b1f53557b50ef65b11e8f

---
+++ assets/27-10-2024-BxWDAImw.js
@@ -0,0 +1,15 @@
+const e=`<h1>Building my website</h1>
+<h2>Project aims</h2>
+<p>To build this website! The goal is to create a simple portfolio/blog that showcases who I am. This project will allow me to post individual articles and display them in an easy-to-use layout, and there will also be components that lead to my LinkedIn and GitHub profiles, as well as a page that gives visitors a background into who I am.</p>
+<p>The project needs to be scalable, allowing me to add new features in the future without affecting the rest of the website, and also needs to have a responsive design, meaning it is viewable on all browsers, as well as pc's, phones, and tablets.</p>
+<h2>Technology</h2>
+<p>This website is built using:</p>
+<ul>
+<li>JavaScript</li>
+<li>React</li>
+<li>Vite</li>
+<li>Tailwind</li>
+<li>Markdown</li>
+<li>GitHub Pages</li>
+</ul>
+`;export{e as default};
