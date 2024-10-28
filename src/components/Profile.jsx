d3480f3f53530a955450e664c54f7f00d4c82135
JimzSnowby <117113270+JimzSnowby@users.noreply.github.com>
2024-10-28 16:41:50 +0000

    Fixed PP display for mobile

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,28 +1,25 @@
 import { Link } from "react-router-dom";
-import profilePic from "../assets/ProfilePic.jpg";
-
+import ProfilePic from "../assets/ProfilePic.jpg";
 
 function Profile() {
   return (
     <div className="group relative w-full max-w-screen-md mx-auto mb-6">
       {/* Glowing gradient border with scale effect */}
-      <div
-        className="absolute -inset-1 bg-gradient-to-r from-malibu-500 to-malibu-400 rounded-lg blur opacity-25 transition-all duration-500 group-hover:opacity-100 group-hover:scale-110"
-      ></div>
+      <div className="absolute -inset-1 bg-gradient-to-r from-malibu-500 to-malibu-400 rounded-lg blur opacity-25 transition-all duration-500 group-hover:opacity-100 group-hover:scale-110"></div>
 
       {/* Main content with padding and scale effect */}
-      <div
-        className="relative px-7 py-6 bg-malibu-900 ring-1 ring-gray-900/5 rounded-lg flex items-center space-x-4 transition-transform transform group-hover:scale-105 duration-500 z-10"
-      >
-        {/* Profile image */}
-        <img 
-          src={profilePic}
-          alt="James Sowerby" 
-          className="w-[200px] h-[200px] rounded-full object-cover"
-        />
+      <div className="relative px-7 py-6 bg-malibu-900 ring-1 ring-gray-900/5 rounded-lg flex flex-col md:flex-row items-center md:items-start space-y-4 md:space-y-0 md:space-x-4 transition-transform transform group-hover:scale-105 duration-500 z-10">
         
-        <div>
-          <h2 className="text-2xl font-semibold text-malibu-400">Hi, I'm James Sowerby</h2>
+        {/* Image */}
+        <img
+          src={ProfilePic}
+          alt="Profile Picture"
+          className="w-[250px] h-[250px] rounded-full"
+        />
+
+        {/* Text Content */}
+        <div className="text-center md:text-left">
+          <h2 className="text-2xl font-semibold mb-2 text-malibu-400">Hi, I'm James Sowerby</h2>
           <p>A Computer Science graduate from Northumbria University.</p>
           <p>For more information about who I am, click the About button at the top of the page, or connect with me on <Link to="https://www.linkedin.com/in/james-sowerby-393146248/" className="text-blue-400 underline">LinkedIn!</Link></p>
         </div>
