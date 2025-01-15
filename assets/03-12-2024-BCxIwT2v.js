11f2d93bc46116e144fbc4c03cad20eba6127aaf
JimzSnowby <JimzSnowby@users.noreply.github.com>
2025-01-15 20:46:11 +0000

    deploy: 7545aa0e68e96bd2f694e4b30e307efd417ac54e

--- assets/03-12-2024-BCxIwT2v.js
+++
@@ -1,3 +0,0 @@
-const e=`<h1>Working On Mindset - Part 2</h1>
-<p>After a week of sticking to my new daily schedule, and having reflected on my strengths and core values, things have been alot easier for me and the feelings of guilt I used to have when not doing job applications have become much less prominent. Things are still a work in progress, however I have a new energy to myself that I haven't had for a long time.</p>
-`;export{e as default};
